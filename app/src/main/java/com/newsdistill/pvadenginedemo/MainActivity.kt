dffb5f61f1cfe4b4dbb5d6d586036121307f7521
Farman <farman.khan@verse.in>
2022-08-31 14:41:33 +0530

    fixed build issue for initial launch

--- app/src/main/java/com/newsdistill/pvadenginedemo/MainActivity.kt
+++ app/src/main/java/com/newsdistill/pvadenginedemo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.newsdistill.pvadenginedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.newshunt.adengine.domain.controller.GetAdUsecaseController
+import com.newshunt.adengine.view.helper.AdsHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("panda: PVAd demo launched....")
     }
 }
