13338b71a0e99038d41ac1d116df692f67e0303f
Farman <farman.khan@verse.in>
2022-09-07 13:07:23 +0530

    added list ad on home page

---
+++ app/src/main/java/com/newsdistill/pvadenginedemo/ads/AdZoneType.kt
@@ -0,0 +1,11 @@
+package com.newsdistill.pvadenginedemo.ads
+
+enum class AdZoneType {
+    LIST_AD_IMAGE,
+    LIST_AD_HTML,
+    LIST_AD_NATIVE,
+    LIST_AD_VIDEO,
+
+    PGI_IMAGE,
+    PGI_HTML,
+}
