85053908b64a4846c71dc2b71ac04fadac5f4d7c
Kush Saini <kush.saini@verse.in>
2022-09-12 16:55:35 +0530

    removed vp-players

--- vp-players/src/main/java/com/dailyhunt/tv/players/helpers/PlayerStateReceiver.kt
+++
@@ -1,5 +0,0 @@
-package com.dailyhunt.tv.players.helpers
-
-import com.dailyhunt.tv.players.entity.PLAYER_STATE
-
-data class PlayerEvent(val playerState: PLAYER_STATE, val id: String?, val msg: String? = null)
