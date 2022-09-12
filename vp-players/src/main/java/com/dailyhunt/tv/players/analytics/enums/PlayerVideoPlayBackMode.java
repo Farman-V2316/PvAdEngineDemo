15230b5eb25043d8d19460e1d34ac9ba2d90ecec
Kush Saini <kush.saini@verse.in>
2022-09-12 16:55:35 +0530

    removed vp-players

--- vp-players/src/main/java/com/dailyhunt/tv/players/analytics/enums/PlayerVideoPlayBackMode.java
+++
@@ -1,8 +0,0 @@
-package com.dailyhunt.tv.players.analytics.enums;
-
-/**
- * Created by umesh.isran on 12/2/16.
- */
-public enum PlayerVideoPlayBackMode {
-  AUTOPLAY, CLICKTOPLAY, SCROLLTOPLAY, SWIPETOPLAY, AUTOPLAY_INLIST, AUTOPLAY_INDETAIL, UNKNOWN
-}
